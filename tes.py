ec6453610f92bf8caa7350ef4fc89ad543ac8d22
Erik Schmidt <erikschmidt3001@gmail.com>
2011-12-03 15:14:11 +0200

    Proxy actually works!

--- tes.py
+++ tes.py
@@ -1,17 +1,19 @@
-import socket, SocketServer
+import socket, SocketServer, urllib, htmllib
 
 bufsize = 8192
 
 class TCPHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
 		client_address = self.client_address
-		while True:
-			client_data = self.request.recv(bufsize)
-                        header = get_header(self, client_data)
-                        print 'Got ' + header[1] + ' from ' + str(client_address)
-			self.request.send(header[1])
+		#while True:
+		client_data = self.request.recv(bufsize)
+                header = get_header(client_data)
+                print 'Got ' + header[1] + ' from ' + str(client_address)
+                print 'Now trying to fetch the desired website.'
+                website = get_website(header[1])
+        	self.request.send(website)
 
-def get_header(self, request):
+def get_header(request):
         while True:
                 line_end = request.find('\n')
                 if line_end != -1:
@@ -20,18 +22,35 @@ def get_header(self, request):
         data = request[:line_end+1].split()
         return data
 
-def get_http(self, url):
-        html = urlopen(url)
+def get_website(url):
+        try:
+                urlhandler = urllib.urlopen(url)
+                html = urlhandler.read()
+        except IOError:
+                print 'Can\'t establish a connection to: ' + url
+                html = 'IOError'
+        except:
+                print 'Some other exception occurred.'
+                html = 'Other exception'
         return html
+
+def annoying(html_input):
+        
+        return annoying_output
 
 def start_server(host='127.0.0.1', port=8080, timeout=10,
                  handler=TCPHandler, mode='n'):
-        tcp_server = SocketServer.ThreadingTCPServer((host, port), handler)
+        tcp_server = SocketServer.TCPServer((host, port), handler)
         print 'Proxy is available at %s:%d' %(host, port)
         tcp_server.serve_forever()
                         
 
 if __name__ == '__main__':
-        import sys
-        
-        start_server(mode=sys.argv[1])
+        start_server()
+#        import sys
+#        try:
+#                start_server(mode=sys.argv[1])
+#        except KeyboardInterrupt:
+#                print 'Keyboard Interrupt'
+#        except:
+#                print 'Usage: proxy [a|n] - annoying | normal'
